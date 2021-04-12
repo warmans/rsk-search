fda813fb782f1056324f219179efaa6097e1eae3
warmans <stefan.warman@gmail.com>
2021-04-12 20:47:23 +0200

    Implemented dark mode

--- gui/src/app/root/component/root/root.component.ts
+++ gui/src/app/root/component/root/root.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { Claims, SessionService } from '../../../module/core/service/session/session.service';
 import { takeUntil } from 'rxjs/operators';
@@ -8,13 +8,15 @@ import { takeUntil } from 'rxjs/operators';
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss']
 })
-export class RootComponent implements OnDestroy {
+export class RootComponent implements OnInit, OnDestroy {
 
   loggedInUser: Claims;
 
+  darkTheme: boolean = false;
+
   destory$: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor(private router: Router, private session: SessionService) {
+  constructor(private renderer: Renderer2, private router: Router, private session: SessionService) {
     session.onTokenChange.pipe(takeUntil(this.destory$)).subscribe((token) => {
       if (token) {
         this.loggedInUser = this.session.getClaims();
@@ -23,14 +25,6 @@ export class RootComponent implements OnDestroy {
       }
     });
   }
-
-  // @HostListener('document:click', ['$event.path'])
-  // public onGlobalClick(targetElementPath: Array<any>) {
-  //   let elementRefInPath = targetElementPath.find(e => e === this.sideNav.nativeElement);
-  //   if (!elementRefInPath) {
-  //     this.showSideNav = false;
-  //   }
-  // }
 
   executeSearch(query: string) {
     this.router.navigate(['/search'], { queryParams: { q: query } });
@@ -45,6 +39,28 @@ export class RootComponent implements OnDestroy {
   ngOnDestroy(): void {
     this.destory$.next(true);
     this.destory$.complete();
+  }
+
+  toggleDarkmode() {
+    this.darkTheme = !this.darkTheme;
+    this.updateTheme();
+  }
+
+  updateTheme() {
+    if (this.darkTheme) {
+      this.renderer.removeClass(document.body, 'light-theme');
+      this.renderer.addClass(document.body, 'dark-theme');
+      localStorage.setItem("theme", "dark");
+    } else {
+      this.renderer.removeClass(document.body, 'dark-theme');
+      this.renderer.addClass(document.body, 'light-theme');
+      localStorage.setItem("theme", "light");
+    }
+  }
+
+  ngOnInit(): void {
+    this.darkTheme = localStorage.getItem("theme") === "dark";
+    this.updateTheme();
   }
 
 }
