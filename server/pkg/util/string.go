75d8555b23352a79c57818375330da6309b3887d
warmans <stefan.warman@gmail.com>
2021-10-13 20:42:18 +0200

    Added cache for spotify data

--- server/pkg/util/string.go
+++ server/pkg/util/string.go
@@ -1,2 +1,17 @@
 package util
 
+import "strings"
+
+func StripNonAlphanumeric(s string) string {
+	var result strings.Builder
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		if ('a' <= b && b <= 'z') ||
+			('A' <= b && b <= 'Z') ||
+			('0' <= b && b <= '9') ||
+			b == ' ' {
+			result.WriteByte(b)
+		}
+	}
+	return result.String()
+}
