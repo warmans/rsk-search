5fd7162993674ea196602a6545b42be2fd22e622
warmans <stefan.warman@gmail.com>
2022-07-31 17:11:46 +0200

    Sync latest data

---
+++ server/pkg/store/rw/migrations/1659280094_merge_changes.sql
@@ -0,0 +1,1 @@
+UPDATE transcript_change SET merged=true WHERE id IN ('PzpZht4kf9QmgAEX9QVMth', 'NJ29MiDr8SPasR9qeFVhgX', 'BvMJNacfKyzdyRkexvW7uP', 'YG5ZaQeMFJvcsgEcbQoniQ', '4XG4FLdMtkcf9Yzx7eAtNX');
