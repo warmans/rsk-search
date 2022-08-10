242274c29d5133730c5022ff2a0286289751feb7
warmans <stefan.warman@gmail.com>
2022-08-10 22:29:18 +0200

    removed trivia box from episode header

--- gui/src/app/module/shared/component/trivia/trivia.component.ts
+++ gui/src/app/module/shared/component/trivia/trivia.component.ts
@@ -8,16 +8,33 @@ import { RskTrivia } from '../../../../lib/api-client/models';
 })
 export class TriviaComponent implements OnInit {
 
+
   @Input()
   episodeID: string;
 
   @Input()
-  trivia: RskTrivia[];
+  set trivia(value: RskTrivia[]) {
+    this._trivia = value;
+    this.triviaTitles = value.map(v => this.simpleTrivia(v.description));
+  }
+
+  get trivia(): RskTrivia[] {
+    return this._trivia;
+  }
+
+  private _trivia: RskTrivia[];
+
+  triviaTitles: string[] = [];
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  simpleTrivia(original: string): string {
+    const lines = (original.trim().split('\n') || []);
+    return lines.length === 0 ? 'NA' : lines[0];
   }
 
 }
