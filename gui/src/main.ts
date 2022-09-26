e5946c1735f6a9594f01e1dd79143abff8ddebd7
warmans <stefan.warman@gmail.com>
2022-09-26 21:51:21 +0200

    Enabled sentry error tracking. Copy fixes

--- gui/src/main.ts
+++ gui/src/main.ts
@@ -1,8 +1,25 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { RootModule } from './app/root/root.module';
 import { environment } from './environments/environment';
+
+import * as Sentry from '@sentry/angular';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: 'https://8992b69abcda4231821c0697176ce365@o1428053.ingest.sentry.io/6777807',
+  integrations: [
+    new BrowserTracing({
+      tracingOrigins: ['localhost', 'https://yourserver.io/api'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.25,
+});
 
 if (environment.production) {
   enableProdMode();
