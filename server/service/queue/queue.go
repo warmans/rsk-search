1c08fbf6149dd23e9a0ae192b3603829288555db
warmans <stefan.warman@gmail.com>
2022-09-27 21:37:44 +0200

    Set max retries for speech-to-text to 2

--- server/service/queue/queue.go
+++ server/service/queue/queue.go
@@ -123,7 +123,7 @@ func (q *ImportQueue) DispatchMachineTranscribe(ctx context.Context, tscriptImpo
 		return err
 	}
 	q.logger.Debug("Enqueue machine transcribe...")
-	_, err = q.client.EnqueueContext(ctx, asynq.NewTask(TaskImportMachineTranscribe, payload, asynq.Timeout(time.Hour*3)))
+	_, err = q.client.EnqueueContext(ctx, asynq.NewTask(TaskImportMachineTranscribe, payload, asynq.Timeout(time.Hour*3), asynq.MaxRetry(2)))
 	return err
 }
 
