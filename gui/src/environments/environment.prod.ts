f30d5fedd5e519d5408043a4441253d0e55965c7
SW <1299401+warmans@users.noreply.github.com>
2022-10-31 19:15:26 +0100

    Reduce required points for reward to 8 (#26)

--- gui/src/environments/environment.prod.ts
+++ gui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  contributionAwardThreshold: 10,
+  contributionAwardThreshold: 8,
 };
--- gui/src/environments/environment.ts
+++ gui/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  contributionAwardThreshold: 10,
+  contributionAwardThreshold: 8,
 };
 
 /*
