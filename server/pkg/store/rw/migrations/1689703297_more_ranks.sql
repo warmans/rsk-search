c2c76e05467a4cbfd5d748d6d9329e473d649ad6
Stefan Warman <stefan.warman@gmail.com>
2023-07-18 19:13:55 +0100

    Added ranks, sync changes

---
+++ server/pkg/store/rw/migrations/1689703297_more_ranks.sql
@@ -0,0 +1,5 @@
+INSERT INTO rank (id, name, points)
+VALUES (uuid_generate_v4(), 'Every Boy''s Fantasy', 350),
+       (uuid_generate_v4(), 'Split Tennis Ball', 400),
+       (uuid_generate_v4(), 'Elegant Boy', 450),
+       (uuid_generate_v4(), 'Andrew Phillips', 500);
