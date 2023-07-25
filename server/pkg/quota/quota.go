14ad4608827e0cfb1f1e712647bfb326ed96358f
Stefan Warman <stefan.warman@gmail.com>
2023-07-25 19:54:45 +0100

    Expanded quotas. Sync prod changes

--- server/pkg/quota/quota.go
+++ server/pkg/quota/quota.go
@@ -1,6 +1,6 @@
 package quota
 
-const BandwidthQuotaInMiB int64 = 1024 * 1000 // 1000 Gib
+const BandwidthQuotaInMiB int64 = 1024 * 2000 // 2000 Gib
 
 func BytesAsMib(numBytes int64) int64 {
 	return numBytes / 1048576
---
+++ server/pkg/store/rw/migrations/1690311215_merge_changes.sql
@@ -0,0 +1,1 @@
+UPDATE transcript_change SET merged=true WHERE id IN ('DTQNQ6icDKF3ZoUnnkCy3h');
