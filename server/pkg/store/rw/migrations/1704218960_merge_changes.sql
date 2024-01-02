08b2de4a0ed8aa6f053097d3ea905329b3a77553
Stefan Warman <stefan.warman@gmail.com>
2024-01-02 18:10:00 +0000

    Sync latest changes

---
+++ server/pkg/store/rw/migrations/1704218960_merge_changes.sql
@@ -0,0 +1,1 @@
+UPDATE transcript_change SET merged=true WHERE id IN ('LqarwVngKtZf7kgdJBCmgL', 'zpZPNKaa4SvydCzCXYnGHE', 'f3bmBQR6VFzRFCqyYnocWN', 'QdsxffDR9LxsZzxHAP6PNW', '2u3yTJcDTLfv5kSNqkuWrH');
