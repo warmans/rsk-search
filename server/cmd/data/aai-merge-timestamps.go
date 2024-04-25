c113da753b88c9f583d37f4f874703fbd2a159ab
Stefan Warman <stefan.warman@gmail.com>
2024-04-25 19:04:45 +0100

    Fixed timestamps across various episodes

--- server/cmd/data/aai-merge-timestamps.go
+++ server/cmd/data/aai-merge-timestamps.go
@@ -132,7 +132,8 @@ func mergeTimestampsTo(target []models.Dialog, compare []models.Dialog, debugPos
 			distance := math.Abs(distancePcnt(targetPos, comparePos, len(target)) - distanceModifier)
 			var matched bool
 			var similarity float64
-			standardMaxDistance := 0.025
+			standardMaxDistance := 0.040
+			
 			if numTargetWords <= 3 {
 				// compare the original text instead
 				similarity = calculateSimilarity(targetLine.Content, compare[comparePos].Content)
