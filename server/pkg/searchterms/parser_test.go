2bd47a52f171625d98650506c70ac12e88c78727
Stefan Warman <stefan.warman@gmail.com>
2024-05-11 12:05:26 +0100

    Fixed tests

--- server/pkg/searchterms/parser_test.go
+++ server/pkg/searchterms/parser_test.go
@@ -1,6 +1,7 @@
 package searchterms
 
 import (
+	"github.com/warmans/rsk-search/pkg/filter"
 	"reflect"
 	"testing"
 )
@@ -17,48 +18,48 @@ func TestMustParse(t *testing.T) {
 		{
 			name: "parse word",
 			args: args{s: "foo"},
-			want: []Term{{Field: "content", Value: "foo", Op: CompOpFuzzyLike}},
+			want: []Term{{Field: "content", Value: "foo", Op: filter.CompOpFuzzyLike}},
 		},
 		{
 			name: "parse words",
 			args: args{s: "foo bar baz"},
-			want: []Term{{Field: "content", Value: "foo bar baz", Op: CompOpFuzzyLike}},
+			want: []Term{{Field: "content", Value: "foo bar baz", Op: filter.CompOpFuzzyLike}},
 		},
 		{
 			name: "parse quoted string",
 			args: args{s: `"foo bar"`},
-			want: []Term{{Field: "content", Value: "foo bar", Op: CompOpEq}},
+			want: []Term{{Field: "content", Value: "foo bar", Op: filter.CompOpEq}},
 		},
 		{
 			name: "parse quoted strings",
 			args: args{s: `"foo bar" "baz"`},
 			want: []Term{
-				{Field: "content", Value: "foo bar", Op: CompOpEq},
-				{Field: "content", Value: "baz", Op: CompOpEq},
+				{Field: "content", Value: "foo bar", Op: filter.CompOpEq},
+				{Field: "content", Value: "baz", Op: filter.CompOpEq},
 			},
 		},
 		{
 			name: "parse publication",
 			args: args{s: `~xfm`},
 			want: []Term{
-				{Field: "publication", Value: "xfm", Op: CompOpEq},
+				{Field: "publication", Value: "xfm", Op: filter.CompOpEq},
 			},
 		},
 		{
 			name: "parse mention",
 			args: args{s: `@steve`},
 			want: []Term{
-				{Field: "actor", Value: "steve", Op: CompOpEq},
+				{Field: "actor", Value: "steve", Op: filter.CompOpEq},
 			},
 		},
 		{
 			name: "parse all",
 			args: args{s: `@steve ~xfm "man alive" karl`},
 			want: []Term{
-				{Field: "actor", Value: "steve", Op: CompOpEq},
-				{Field: "publication", Value: "xfm", Op: CompOpEq},
-				{Field: "content", Value: "man alive", Op: CompOpEq},
-				{Field: "content", Value: "karl", Op: CompOpFuzzyLike},
+				{Field: "actor", Value: "steve", Op: filter.CompOpEq},
+				{Field: "publication", Value: "xfm", Op: filter.CompOpEq},
+				{Field: "content", Value: "man alive", Op: filter.CompOpEq},
+				{Field: "content", Value: "karl", Op: filter.CompOpFuzzyLike},
 			},
 		},
 	}
