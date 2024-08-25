4e76afe2a77503f0c8ee497ef2028c25d2456508
warmans <>
2024-08-25 16:19:34 +0100

    Sync prod changes

---
+++ server/pkg/store/rw/migrations/1724599154_merge_changes.sql
@@ -0,0 +1,1 @@
+UPDATE transcript_change SET merged=true WHERE id IN ('ZmqHo8wYn9CECjFAiw5Cea', 'xUJfvtzGu5AEDUB9MDgii7', 'kNT9LVwzexbMBNXC8nhL5j', 'VYwE3NTG8fcmqiBPmdjfTc', '7Lho5AWWZmAfQvyJUwk28P', 'gfmH4iWYJRBbxiQ7wxqpuh', 'myJShNqVHQ868XUUZxzih6', 'u8F2diWXaTuRfbftTtWNUb');
