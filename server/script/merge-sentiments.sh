abb0c3a74d0faf2b2d1643eecfcc02bf76c08924
warmans <stefan.warman@gmail.com>
2025-05-15 21:42:32 +0100

    Refresh sentiments

--- server/script/merge-sentiments.sh
+++ server/script/merge-sentiments.sh
@@ -2,6 +2,6 @@
 
 for S in {1..2}; do
   for E in {1..51};
-    do EP=$(printf "S%sE%02d" "${S}" "${E}"); stat "var/aai-transcripts/xfm-${EP}.mp3?remastered=1.json" && ./bin/rsk-search data aai-merge-sentiments -s "var/aai-transcripts/xfm-${EP}.mp3?remastered=1.json" -t "ep-xfm-${EP}.json";
+    do EP=$(printf "S%sE%02d" "${S}" "${E}"); stat "var/aai-transcripts/xfm-${EP}.mp3?remastered=1.json" 1>/dev/null 2>/dev/null && ./bin/rsk-search data aai-merge-sentiments -s "var/aai-transcripts/xfm-${EP}.mp3?remastered=1.json" -t "ep-xfm-${EP}.json";
     done;
 done;
