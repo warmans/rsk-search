08c1c371994a321d07bd939d30d410d1a3e8769f
warmans <stefan.warman@gmail.com>
2025-05-18 16:54:41 +0100

    Added podcast S2

--- server/cmd/data/init-from-audio-files.go
+++ server/cmd/data/init-from-audio-files.go
@@ -6,12 +6,10 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/warmans/rsk-search/pkg/data"
 	"github.com/warmans/rsk-search/pkg/models"
-	"github.com/warmans/rsk-search/pkg/util"
 	"go.uber.org/zap"
 	"os"
-	"os/exec"
 	"path"
-	"sort"
+	"strings"
 	"time"
 )
 
@@ -22,14 +20,16 @@ type audioFile struct {
 	year        int
 	publication string
 	summary     string
+	series      int32
+	episode     int32
 }
 
+// InitFromAudioFilesCmd e.g. ./script/meta-from-audio-files.sh ${AUDIO_DIR}/podcast-S2*
 func InitFromAudioFilesCmd() *cobra.Command {
 
-	var rawAudioDir string
+	var publicationType string
+	var audioFilePath string
 	var publication string
-	var series int32
-	var episodeOffset int32
 	var dryRun bool
 
 	cmd := &cobra.Command{
@@ -43,57 +43,29 @@ func InitFromAudioFilesCmd() *cobra.Command {
 				}
 			}()
 
-			audioFiles := []audioFile{}
+			if audioFilePath == "" {
+				return fmt.Errorf("audio file path not specified")
+			}
 
-			entries, err := os.ReadDir(rawAudioDir)
+			logger.Info(fmt.Sprintf("processing %s", audioFilePath))
+
+			meta, err := parseMetadata(logger, audioFilePath, publication)
 			if err != nil {
 				return err
 			}
-			for _, v := range entries {
-				if v.IsDir() {
-					continue
-				}
-				filePath := path.Join(rawAudioDir, v.Name())
 
-				logger.Info(fmt.Sprintf("processing %s", filePath))
-
-				meta, err := parseMetadata(logger, filePath, publication)
-				if err != nil {
-					logger.Warn(fmt.Sprintf("Failed to parse id3 of %s, giving up: %s", v.Name(), err.Error()))
-					continue
-				}
-				audioFiles = append(audioFiles, meta)
-			}
-
-			sort.Slice(audioFiles, func(i, j int) bool {
-				return audioFiles[i].year < audioFiles[j].year
-			})
-
-			if dryRun {
-				for _, f := range audioFiles {
-					logger.Info("Created file", zap.String("name", f.name), zap.Timep("date", f.date), zap.Int("year", f.year))
-				}
-				return nil
-			}
-
-			for k, f := range audioFiles {
-				ep, err := initEpisodeFileFromAudio(logger, f, series, episodeOffset+int32(k)+1, cfg.dataDir)
-				if err != nil {
-					return fmt.Errorf("failed to init file for file %s date: %s name: %s: %w", f.path, f.date, f.name, err)
-				}
-				if _, err := exec.Command("cp", f.path, path.Join(cfg.audioDir, fmt.Sprintf("%s.mp3", ep.ShortID()))).CombinedOutput(); err != nil {
-					return err
-				}
+			_, err = initEpisodeFileFromAudio(logger, meta, cfg.dataDir, models.PublicationType(publicationType))
+			if err != nil {
+				return fmt.Errorf("failed to init metadata for file %s date: %s name: %s: %w", meta.path, meta.date, meta.name, err)
 			}
 			return nil
 		},
 	}
 
-	cmd.Flags().StringVarP(&rawAudioDir, "raw-audio-path", "", "", "Path to scan")
-	cmd.Flags().StringVarP(&publication, "publication", "p", "other", "Publication to give episodes")
+	cmd.Flags().StringVarP(&audioFilePath, "audio-file-path", "", "", "Path to scan")
+	cmd.Flags().StringVarP(&publication, "publication", "p", "", "Publication to give episodes")
+	cmd.Flags().StringVarP(&publicationType, "publication-type", "t", "podcast", "Publication type to give episodes")
 	cmd.Flags().BoolVarP(&dryRun, "dry-run", "x", false, "don't write any files, just log")
-	cmd.Flags().Int32VarP(&series, "series", "s", 1, "use this as the series number in meta/renamed file")
-	cmd.Flags().Int32VarP(&episodeOffset, "episode-num-offset", "e", 0, "use this as the first episode number in meta/renamed file")
 	return cmd
 }
 
@@ -107,6 +79,12 @@ func parseMetadata(logger *zap.Logger, fileName string, publication string) (aud
 		return audioFile{}, err
 	}
 	rawTags := tags.Raw()
+
+	// try and get the publication from the file name e.g. podcast-S1E01.mp3
+	filenameParts := strings.Split(path.Base(fileName), "-")
+	if publication == "" && len(filenameParts) == 2 {
+		publication = filenameParts[0]
+	}
 
 	result := audioFile{}
 	result.publication = publication
@@ -127,6 +105,13 @@ func parseMetadata(logger *zap.Logger, fileName string, publication string) (aud
 		startOfYear := time.Date(result.year, 0, 0, 0, 0, 0, 0, time.UTC)
 		result.date = &startOfYear
 	}
+
+	parsedSeries, parsedEpisode, err := models.ExtractSeriesAndEpisode(strings.TrimSuffix(fileName, ".mp3"))
+	if err != nil {
+		return result, err
+	}
+	result.series = parsedSeries
+	result.episode = parsedEpisode
 
 	return result, nil
 }
@@ -156,33 +141,27 @@ func findReleaseTimestamp(tags map[string]interface{}) string {
 func initEpisodeFileFromAudio(
 	logger *zap.Logger,
 	f audioFile,
-	series int32,
-	episode int32,
 	dataDir string,
+	publicationType models.PublicationType,
 ) (*models.Transcript, error) {
 
 	ep := &models.Transcript{
-		Publication: f.publication,
-		Series:      series,
-		Episode:     episode,
-		ReleaseDate: f.date,
-		Name:        f.name,
-		Version:     "0.0.0",
-		Summary:     f.summary,
-		Transcript:  []models.Dialog{},
-		Locked:      true,
+		PublicationType: publicationType,
+		Publication:     f.publication,
+		Series:          f.series,
+		Episode:         f.episode,
+		ReleaseDate:     f.date,
+		Name:            f.name,
+		Version:         "0.0.0",
+		Summary:         f.summary,
+		Transcript:      []models.Dialog{},
+		Locked:          false,
 		Meta: map[models.MetadataType]string{
 			models.CoverArtURL: "/assets/cover/default.jpg",
 		},
-	}
-
-	filePath := data.EpisodeFileName(dataDir, ep)
-	if ok, err := util.FileExists(filePath); ok || err != nil {
-		logger.Info("Exists...", zap.String("path", filePath))
-		if ok && err == nil {
-			err = fmt.Errorf("file already exists")
-		}
-		return nil, err
+		Media: models.Media{
+			AudioFileName: path.Base(f.path),
+		},
 	}
 
 	logger.Info("Creating...", zap.String("episode", f.name))
---
+++ server/script/meta-from-audio-files.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+for f in "$@";
+ do echo "Processing... ${f}" && ./bin/rsk-search data init-from-audio --audio-file-path="${f}";
+done;
